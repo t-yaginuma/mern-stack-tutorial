1daefbf7860739e6299825d3e10904f2fee9a4dd
t-yaginuma <toru45s@gmail.com>
2024-11-24 10:14:28 +0900

    update for production build

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,22 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
-
+import path from "path";
 dotenv.config();
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // arrows us to accept json data in request body
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+}
 
 app.listen(PORT, () => {
   connectDB();
